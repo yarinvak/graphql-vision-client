1f7dd507cee947f9fc13e5f862f71e2b6ea10466
Yarin <yarinvak@gmail.com>
2020-02-01 16:52:57 +0200

    support keepalive

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import {ApolloServer} from 'apollo-server-express';
 import express from 'express';
 import cors from 'cors';
 import GraphQLVisionPlugin from 'graphql-vision-plugin';
+import {keepAliveInterval} from 'graphql-vision-plugin';
 
 const books = [
     {
@@ -36,10 +37,12 @@ const app = express();
 app.get('/keepAlive', cors(), (req, res) => res.sendStatus(200));
 
 const server = new ApolloServer({
-    typeDefs, resolvers, tracing: true, plugins: [new GraphQLVisionPlugin('http://localhost:4003/graphql')]
+    typeDefs, resolvers, tracing: true, plugins: [new GraphQLVisionPlugin('http://localhost:4000/graphql', 'yarin-try')]
 });
 
 server.applyMiddleware({app});
+
+keepAliveInterval();
 
 const port = 4002;
 app.listen(port, (() => {
